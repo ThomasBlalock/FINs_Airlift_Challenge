fixed NaN in maksing issue permanently, refactored heur-pretrain to work with new masking

heuristic_pretraining.py
@@ -255,18 +255,18 @@ def calculate_batch_loss(pi, criterion, x_batch, target_batch):
     action_loss = criterion(y_batch['action_logits'].view(-1, 3), target_batch['actions'].view(-1, 3))
 
     # Destination Loss
-    dest_mask = (target_batch['actions'][:, :, 0] == 1).float().unsqueeze(-1)\
-        .expand(-1, -1, x_batch['nodes']['PyGeom'][0].x.shape[0])
+    dest_mask = (target_batch['actions'][:, 0] == 1).float().unsqueeze(-1)\
+        .expand(-1, x_batch['nodes']['PyGeom'][0].x.shape[0]).unsqueeze(0)
     destination_loss = criterion(
         torch.nan_to_num(y_batch['destination_logits'], nan=0)*dest_mask,
         target_batch['destinations']*dest_mask
     )
     
     # Cargo Loss
-    cargo_mask = (target_batch['actions'][:, :, 1] == 1).float()
-    cargo_mask = torch.tensor([y+[1] for y in cargo_mask.tolist()])
-    cargo_mask = cargo_mask.unsqueeze(-1).expand(-1, -1, y_batch['cargo'].shape[1])
-    cargo_mask = cargo_mask.permute(0, 2, 1)
+    cargo_mask = (target_batch['actions'][:, 1] == 1).float()
+    cargo_mask = torch.tensor(cargo_mask.tolist()+[1]).float()\
+        .unsqueeze(-1).expand(-1, y_batch['cargo'].shape[1]).unsqueeze(0)\
+            .permute(0, 2, 1)
 
     cargo_loss = criterion(
         torch.nan_to_num(y_batch['cargo_logits'], nan=0)*cargo_mask,
@@ -347,12 +347,9 @@ def heuristic_pretraining(pi, h, optim, epochs=10, num_batches=32, timesteps_per
                         h[env_id].reset(ob)
                 
                 obs[env_id] = ob
-            #TODO: Fix the dimention mis-match when the number of cargo doesn't match
-            x_batch = stack_nested_dicts(x_batch)
-            y_batch = stack_nested_dicts(y_batch)
             total_loss = 0
-            for x_batch, y_batch in zip(x_batch, y_batch):
-                total_loss += calculate_batch_loss(pi, criterion, x_batch, y_batch)
+            for x_sample, y_sample in zip(x_batch, y_batch):
+                total_loss += calculate_batch_loss(pi, criterion, x_sample, y_sample)
 
             # Backward Pass
             optim.zero_grad()
@@ -361,7 +358,7 @@ def heuristic_pretraining(pi, h, optim, epochs=10, num_batches=32, timesteps_per
             optim.step()
         
             epoch_loss += total_loss.item()
-            print(f"Batch Loss: {total_loss.item()}")
+            # print(f"Batch Loss: {total_loss.item()}")
         
         avg_epoch_loss = epoch_loss / num_batches
         print(f"Epoch {epoch+1} Average Loss: {avg_epoch_loss}")
@@ -378,7 +375,7 @@ def main():
     """
 
     seed = 0
-    lr = 0.00000000000000001
+    lr = 0.0001
     num_envs = 4
 
     h = [HeuristicSolution() for _ in range(num_envs)]
@@ -388,7 +385,7 @@ def main():
     optim = torch.optim.Adam(pi.parameters(), lr=lr)
 
     # Pretrain the policy model
-    heuristic_pretraining(pi, h, optim, epochs=10, num_batches=32, timesteps_per_minibatch=32, num_envs=num_envs, seed=seed)
+    heuristic_pretraining(pi, h, optim, epochs=20, num_batches=32, timesteps_per_minibatch=32, num_envs=num_envs, seed=seed)
 
 
 main()

solution/format_obs.py
@@ -157,19 +157,19 @@ def format_obs(obs, t, max_airport_capacity=5e+09, max_weight=10000, max_time=10
 
         # Action Mask
         if airplane.state != READY_FOR_TAKEOFF:
-            action_mask.append([-float('inf'), -float('inf'), 0])
+            action_mask.append([0, 0, 1])
         elif airplane.cargo_at_current_airport == [] and airplane.cargo == {}:
             # If there is no cargo at the airport and no cargo on the plane, mask unload/load option
-            action_mask.append([0, -float('inf'), 0])
+            action_mask.append([1, 0, 1])
         else:
-            action_mask.append([0, 0, 0])
+            action_mask.append([1, 1, 1])
             
 
         # Agents Mask
         if airplane.state == READY_FOR_TAKEOFF: # 1-based indexing
-            agents_mask.append([0 if (i+1) in airplane.available_routes else -float('inf') for i in range(len(airport_list))])
+            agents_mask.append([1 if (i+1) in airplane.available_routes else 0 for i in range(len(airport_list))])
         else:
-            agents_mask.append([0 if (i+1) == airplane.location else -float('inf') for i in range(len(airport_list))])
+            agents_mask.append([1 if (i+1) == airplane.location else 0 for i in range(len(airport_list))])
 
     agents_tensor = torch.tensor(agents_tensor, dtype=torch.float32)
     agents_mask = torch.tensor(agents_mask, dtype=torch.float32)
@@ -205,11 +205,11 @@ def format_obs(obs, t, max_airport_capacity=5e+09, max_weight=10000, max_time=10
         # Cargo Mask
         if t < cargo.earliest_pickup_time:
             if cargo.on_a_plane == 1:
-                cargo_mask.append([1 if airplane_list.airplanes[a]==cargo.location else -float('inf') for a in agents_map] + [1])
+                cargo_mask.append([1 if airplane_list.airplanes[a]==cargo.location else 0 for a in agents_map] + [1])
             else:
-                cargo_mask.append([1 if airplane_list.airplanes[a].location==cargo.location else -float('inf') for a in agents_map] + [1])
+                cargo_mask.append([1 if airplane_list.airplanes[a].location==cargo.location else 0 for a in agents_map] + [1])
         else:
-            cargo_mask.append([-float('inf') for _ in range(len(agents_map))] + [1])
+            cargo_mask.append([0 for _ in range(len(agents_map))] + [1])
     
     cargo_tensor = torch.tensor(cargo_tensor, dtype=torch.float32)
     cargo_mask = torch.tensor(cargo_mask, dtype=torch.float32)

solution/models/v4/model_v4.py
@@ -702,18 +702,33 @@ class PolicyHead(nn.Module):
         actions = actions.argmax(dim=-1)
 
         # Update Destination Mask
-        destination_action_mask = torch.where(actions == DESTINATION_ACTION, torch.tensor(float(0)), torch.tensor(float('-inf')))
+        destination_action_mask = torch.where(actions == DESTINATION_ACTION, torch.tensor(float(1)), torch.tensor(float(0)))
         destination_action_mask = destination_action_mask.unsqueeze(-1).expand(-1, -1, destination_mask.shape[-1])
-        destination_mask = destination_mask + destination_action_mask
+        destination_mask = destination_mask * destination_action_mask
 
         # Update Cargo Mask
-        cargo_action_mask = torch.where(actions == LOAD_UNLOAD_ACTION, torch.tensor(float(0)), torch.tensor(float('-inf')))
-        cargo_action_mask = torch.tensor([y+[0] for y in cargo_action_mask.tolist()])
+        cargo_action_mask = torch.where(actions == LOAD_UNLOAD_ACTION, torch.tensor(float(1)), torch.tensor(float(0)))
+        cargo_action_mask = torch.tensor([y+[1] for y in cargo_action_mask.tolist()])
         cargo_action_mask = cargo_action_mask.unsqueeze(-1).expand(-1, -1, cargo_mask.shape[1])
         cargo_action_mask = cargo_action_mask.permute(0, 2, 1)
-        cargo_mask = cargo_mask + cargo_action_mask
+        cargo_mask = cargo_mask * cargo_action_mask
 
         return destination_mask, cargo_mask
+
+    def normalize(self, logits):
+        """
+        Args:
+            logits: tensor (b x p x n) probabilities
+
+        Returns: tensor (b x p x n) normalized probabilities
+        """
+
+        mask = logits.sum(dim=-1) != 0
+        norm_logits = logits / logits.sum(dim=-1, keepdim=True)
+        norm_logits = torch.where(mask.unsqueeze(-1), norm_logits, torch.zeros_like(logits))
+        assert not torch.isnan(norm_logits).any() and not torch.isinf(norm_logits).any(), 'NaN or Inf in normalized logits'
+
+        return logits
 
     def forward(self, n, p, c, action_mask=None, destination_mask=None, cargo_mask=None):
         """
@@ -746,10 +761,10 @@ class PolicyHead(nn.Module):
         del X
         action_logits = self.transformer_action(p_a)
         action_logits = self.down_projection_action(action_logits)
-        action_logits_detached = action_logits.detach() + action_mask
-        action_logits_detached = nn.Softmax(dim=-1)(action_logits_detached)
         action_logits = nn.Softmax(dim=-1)(action_logits)
-        actions = self.sample(action_logits_detached)
+        action_logits = action_logits * action_mask
+        action_logits = action_logits / action_logits.sum(dim=-1, keepdim=True)
+        actions = self.sample(action_logits)
 
         # Update Masks
         destination_mask, cargo_mask = self.update_masks(
@@ -768,15 +783,11 @@ class PolicyHead(nn.Module):
                 p_d = X['agents']
             if 'cargo' in X:
                 c_d = X['cargo']
-        destination_logits = self.ptr_destination(p_d, n_d)#, mask=destination_mask)#TODO: change this back is your experiment doesnt work
-        destination_logits_detached = destination_logits.detach() + destination_mask
-        destination_logits_detached = nn.Softmax(dim=-1)(destination_logits_detached)
-        mask_bool = torch.all(destination_mask == -float('inf'), dim=-1)
-        destination_logits_detached = \
-            torch.where(~mask_bool.unsqueeze(-1), destination_logits_detached, 
-                        torch.zeros_like(destination_logits_detached))
+        destination_logits = self.ptr_destination(p_d, n_d)
         destination_logits = nn.Softmax(dim=-1)(destination_logits)
-        destinations = self.sample(destination_logits_detached)
+        destination_logits = destination_logits * destination_mask
+        destination_logits = self.normalize(destination_logits)
+        destinations = self.sample(destination_logits)
         del X
 
         # Cargo Head
@@ -795,15 +806,11 @@ class PolicyHead(nn.Module):
             if 'cargo' in X:
                 c_c = X['cargo']
         del X
-        cargo_logits = self.ptr_cargo(c_c, p_c, add_choice=True)#, mask=cargo_mask, add_choice=True) TODO
-        cargo_logits_detached = cargo_logits.detach() + cargo_mask
-        cargo_logits_detached = nn.Softmax(dim=-1)(cargo_logits_detached)
-        mask_bool = torch.all(cargo_mask == -float('inf'), dim=-1)
-        cargo_logits_detached = \
-            torch.where(~mask_bool.unsqueeze(-1), cargo_logits_detached, 
-                        torch.zeros_like(cargo_logits_detached))
+        cargo_logits = self.ptr_cargo(c_c, p_c, add_choice=True)
         cargo_logits = nn.Softmax(dim=-1)(cargo_logits)
-        cargo = self.sample(cargo_logits_detached)
+        cargo_logits = cargo_logits * cargo_mask
+        cargo_logits = self.normalize(cargo_logits)
+        cargo = self.sample(cargo_logits)
 
         return {
             'actions': actions,